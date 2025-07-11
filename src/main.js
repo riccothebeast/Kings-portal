ecdc71a7a7129c04a338aac6f0661869ecd62962
riccothebeast <robbinithai@gmail.com>
2025-07-11 11:50:00 +0300

    moved index.html to parent directory

---
+++ src/main.js
@@ -0,0 +1,8 @@
+var typed= new Typed("#typed", {
+    strings: ["Cybesecurity", "Software Development", "Data Science","Robototics and AI","IoT and Blockchain"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+});
